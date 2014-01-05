95c1aa0a6331f008d04198ea696f162bb5908820
Thomas Eschemann <tom@ubuntu.(none)>
2014-01-04 16:32:21 -0800

    WIP ARP

--- tcpsniff.c
+++ tcpsniff.c
@@ -1,7 +1,7 @@
 #include "tcpsniff.h"
 
 int main(int argc, char *argv[]){
-	char * iflag;
+	char * iflag=NULL;
 	char * oflag;
 	char * fflag;
 	int vflag, pflag=0;
@@ -9,53 +9,63 @@ int main(int argc, char *argv[]){
 
 	while ((c = getopt (argc, argv, "i:o:f:v:p")) != -1){
 		switch(c){
-			case 'p' :
+			case 'p' : // promiscuous mode 
 				pflag = 1;
 				break;
-			case 'i' :
+			case 'i' : // interface to listen on
 				iflag = strdup(optarg);
 				break;
-			case 'o' :
+			case 'o' : // log file
 				oflag = strdup(optarg);
 				break;
-			case 'f' :
+			case 'f' : // BPF filer
 				fflag = strdup(optarg);
 				break;
-			case 'v' :
+			case 'v' : // verbosity level
 				vflag = atoi(optarg);
 				break;
 			default:
 				break;
 		}
 	}
-
+	
 	char error_buffer[PCAP_ERRBUF_SIZE];
 	pcap_t * capture_handle;
+
+	if(iflag==NULL){
+		if ((iflag = pcap_lookupdev(error_buffer))==NULL) {
+			fprintf(stderr, "Couldn't find default device: %s\n", error_buffer);
+			print_all_devices();
+			exit(1);
+		}
+	}	
 
 	if((capture_handle = pcap_create(iflag, error_buffer))==NULL){
 		fprintf(stderr,"Error in pcap_create: %s\n", error_buffer);
 		exit(1);
 	}
 
-	pcap_set_snaplen(capture_handle,65535);
+	pcap_set_snaplen(capture_handle,CAPTURESIZE);
 	if(pflag==1){
 		pcap_set_promisc(capture_handle, 1);
 	}
 
 	if((pcap_activate(capture_handle) == PCAP_ERROR_NO_SUCH_DEVICE)){
 		printf("Device (%s) does not exist\n",iflag);
-		printf("Please use one of the following devices : \n");
+		printf("The following devices are available : \n");
 		print_all_devices();
+		exit(0);
 	}
 
-    if((pcap_loop(capture_handle, 0, (pcap_handler)got_packet, NULL))==-1){
-    	char *prefix = "Error in pcap_loop: ";
-    	pcap_perror(capture_handle, prefix);
-	printf("Error in loop\n");
-    }
+	printf("listening on %s, capture size %d bytes\n",iflag,CAPTURESIZE);
+	if((pcap_loop(capture_handle, 0, (pcap_handler)got_packet, NULL))==-1){
+		char *prefix = "Error in pcap_loop: ";
+		pcap_perror(capture_handle, prefix);
+		printf("Error in loop\n");
+	}
 
-    pcap_close(capture_handle);
-    return 0;
+	pcap_close(capture_handle);
+    	return 0;
 }
 
 void print_header_telnet(const u_char *packet){
@@ -73,12 +83,39 @@ void print_header_ftp(const u_char *packet){
 void print_header_smtp(const u_char *packet){
 	printf("|----> SMTP\n");
 }
+char * get_tcp_flags(struct tcphdr *tcp){
+	char str[100];
+	strcpy(str,"");
+	if(tcp->fin==1){
+		strcat(str,"FIN, ");
+	}
+	if(tcp->syn==1){
+		strcat(str,"SYN, ");
+	}
+	if(tcp->rst==1){
+		strcat(str,"RST, ");
+	}
+	if(tcp->psh==1){
+		strcat(str,"PSH, ");
+	}
+	if(tcp->ack==1){
+		strcat(str,"ACK, ");
+	}
+	if(tcp->urg==1){
+		strcat(str,"URG, ");
+	}
+	return strdup(str);
+}
 void print_header_tcp(const u_char *packet){
 	printf("|---> TCP ");
 	struct tcphdr *tcp;
 	tcp = (struct tcphdr*)packet;
-	printf("%d > %d\n",ntohs(tcp->source), ntohs(tcp->dest));
-	
+	char * flags = get_tcp_flags(tcp);
+	if(strlen(flags) > 2){
+		flags[strlen(flags)-2] = '\0';	
+	}
+	printf("%d > %d, Flags : [%s] \n",ntohs(tcp->source), ntohs(tcp->dest),flags);
+
 	switch(ntohs(tcp->source)){
 		case 80:
 			print_header_http(packet);
@@ -121,15 +158,55 @@ void print_header_tcp(const u_char *packet){
 				default:
 					break;
 			}
-		}
+	}
 }
+void print_header_bootp(const u_char *packet){
+	char magic_cookie[4] = VM_RFC1048;	
+	printf("|----> BOOTP ");
+	struct bootp *bootp;
+	struct vend *vendor;
+	bootp = (struct bootp*) packet;
+	vendor = (struct vend*) bootp->bp_vend;
 
+	if(strncmp(vendor->v_magic,magic_cookie,4)==0){
+		// Vendor specific options available
+	}
 
+}
+void print_header_dns(const u_char *packet){
+	printf("|----> DNS\n");
+}
 void print_header_udp(const u_char *packet){
 	printf("|---> UDP ");
 	struct udphdr *udp;
 	udp = (struct udphdr*)packet;
 	printf("%d > %d\n",ntohs(udp->source),ntohs(udp->dest));
+
+	switch(ntohs(udp->source)){
+		case 67:
+			print_header_bootp((packet+sizeof(struct udphdr)));
+			break;
+		case 68:
+			print_header_bootp((packet+sizeof(struct udphdr)));
+			break;
+		case 53:
+			print_header_dns((packet+sizeof(struct udphdr)));
+			break;
+		default:
+			switch(ntohs(udp->dest)){
+				case 67:
+					print_header_bootp((packet+sizeof(struct udphdr)));
+					break;
+				case 68:
+					print_header_bootp((packet+sizeof(struct udphdr)));
+					break;
+				case 53:
+					print_header_dns((packet+sizeof(struct udphdr)));
+					break;	
+				default:
+					break;
+			}
+	}
 }
 
 void print_header_ip(const u_char *packet){
@@ -140,7 +217,7 @@ void print_header_ip(const u_char *packet){
 	int version = ip_header->ip_v;
 	inet_ntop(AF_INET,&(ip_header->ip_src),src,INET_ADDRSTRLEN);
 	inet_ntop(AF_INET,&(ip_header->ip_dst),dst,INET_ADDRSTRLEN);
-	printf("|--> IPv%d %s > %s \n",version,src,dst);
+	printf("|--> IPv%d %s > %s, Length : %d, TTL : %d \n",version,src,dst,ip_header->ip_len,ip_header->ip_ttl);
 
 	switch(ip_header->ip_p){
 		case 6:
@@ -155,18 +232,57 @@ void print_header_ip(const u_char *packet){
 }
 
 void print_header_arp(const u_char *packet){
-	printf("|--> ARP \n");
-}
+	struct arphdr *arp;
+	arp = (struct arphdr*) packet;
+	u_char * dstip;
+	u_char * srcip;
+	char dstip_r[INET_ADDRSTRLEN];
+	int hl, pl;
+	hl = arp->ar_hln;
+	pl = arp->ar_pln;
+	
+	switch(pl){
+		case 4:
+			dstip = malloc(4*sizeof(u_char));
+			srcip = malloc(4*sizeof(u_char));
+			strncpy(dstip,arp->ar_op+sizeof(u_short)+(2*hl*sizeof(u_char))+pl*sizeof(u_char),4*sizeof(u_char));
+			break;
+		case 16:
+			dstip = malloc(16*sizeof(u_char));
+			srcip = malloc(16*sizeof(u_char));
+			break;
+		default:
+			break;
+	}	
 
-char * uintip_to_string(uint32_t ip){
-	uint8_t  octet[4];
-	char *res = malloc(15*sizeof(int));
-	int x;
-	for (x = 0; x < 4; x++){
-		octet[x] = (ip >> (x * 8)) & (uint8_t)-1;
+	inet_ntop(AF_INET,&(dstip),dstip_r,INET_ADDRSTRLEN);
+	printf("|--> ");
+	switch(ntohs(arp->ar_op)){
+		case ARPOP_REQUEST:
+			printf("ARP request who has %s",dstip_r);
+			break;
+		case ARPOP_REPLY:
+			printf("ARP reply");
+			break;
+		case ARPOP_RREQUEST:
+			printf("RARP request");
+			break;
+		case ARPOP_RREPLY:
+			printf("RARP reply");
+			break;
+		case ARPOP_InREQUEST:
+			printf("InARP request");
+			break;
+		case ARPOP_InREPLY:
+			printf("InARP reply");
+			break;
+		case ARPOP_NAK:
+			printf("ARP NAK");
+			break;
+		default:
+			break;
 	}
-	sprintf(res,"%d.%d.%d.%d",octet[0],octet[1],octet[2],octet[3]);
-	return res;	
+	printf("\n");
 }
 
 void got_packet(u_char *user, const struct pcap_pkthdr *phrd, const u_char *packet){
--- tcpsniff.h
+++ tcpsniff.h
@@ -5,9 +5,12 @@
 #include <unistd.h>
 #include <time.h>
 #include <netinet/if_ether.h>
+#include <net/if_arp.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
+#include "bootp.h"
+#define CAPTURESIZE 65535
 
 int main(int argc, char *argv[]);
 void got_packet(u_char *user, const struct pcap_pkthdr *phrd, const u_char *pdata);
